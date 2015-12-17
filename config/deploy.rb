ca5f60f1a9c71be0eea11f994a0bcedc5212770d
robwannell <rob@meltemiweb.com>
2015-12-17 13:49:08 -0800

    xx

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :repo_url, 'git@github.com:robwannell/darrell.git'
 
 set :deploy_to, '/home/deploy/darrell'
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
