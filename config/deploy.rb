2f0730fc1829b76adb38f8ca7a094e75488f5566
robwannell <rob@meltemiweb.com>
2015-12-18 13:05:33 -0800

    rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,11 @@ set :repo_url, 'git@github.com:robwannell/darrell.git'
 
 set :deploy_to, '/home/deploy/darrell'
 
+set :rbenv_path, '/home/deploy/.rbenv/'
+
+
+
+
 set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
