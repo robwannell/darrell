9275ea6a34225948b20f3054e94b7a7cb25f46f4
robwannell <rob@meltemiweb.com>
2016-01-06 20:49:05 -0800

    aa

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,7 @@
 class ContactMailer < ActionMailer::Base
   default from: 'notifier@steinberg4sac.com'
           
-  @recip = MailRecipient.find.(2)
   
-  default to: @recip.email
 
     def contact_email(name, email, body)
         @name = name
