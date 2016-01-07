ebbff8feb949f069d4858f53736d7e16a60e7888
robwannell <rob@meltemiweb.com>
2016-01-06 20:50:40 -0800

    ema

--- app/mailers/support_mailer.rb
+++ app/mailers/support_mailer.rb
@@ -1,6 +1,7 @@
 class SupportMailer < ActionMailer::Base
   default from: 'notifier@steinberg4sac.com'
-  
+  @recip = MailRecipient.find.(1)
+  default to: @recip.email
     
     def support_email(name, email, phone, address, body, help)
         @name = name
