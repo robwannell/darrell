0f9049dd93bcc1c3e59679b36b19fdfb34893bd6
robwannell <rob@meltemiweb.com>
2016-01-08 08:22:58 -0800

    news change

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,5 +1,5 @@
 class News < ActiveRecord::Base
-  has_attached_file :avatar, styles: { medium: "300x200#{}>", thumb: "150x100#" }, default_url: "news/:styles/missing.jpg"
+  has_attached_file :avatar, styles: { medium: "710x400#", thumb: "150x100#" }, default_url: "news/:styles/missing.jpg"
     validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   
   extend FriendlyId
