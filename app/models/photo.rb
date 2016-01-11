6a605053be33c3aa237a01749fbd721636208b6f
robwannell <rob@meltemiweb.com>
2016-01-11 13:04:14 -0800

    Hide community leaders

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ActiveRecord::Base
   
   
-  has_attached_file :avatar, styles: { medium: "710x400", thumb: "150x100" }
+  has_attached_file :avatar, styles: { medium: "680x400", thumb: "150x100" }
   
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   validates :avatar, presence: true
