db0eb6b312e2efbf8e72a65a6d9c4fe34c48fdc3
robwannell <rob@meltemiweb.com>
2016-01-12 13:03:07 -0800

    news10

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -4,7 +4,7 @@ class NewsController < ApplicationController
   # GET /news
   # GET /news.json
   def index
-    @news = News.paginate(:page => params[:page], :per_page => 2)
+    @news = News.paginate(:page => params[:page], :per_page => 10)
   end
 
   # GET /news/1
