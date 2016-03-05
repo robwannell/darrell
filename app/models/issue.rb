2ae3207501bf53f01daa9e21988982b3b792c989
robwannell <rob@meltemiweb.com>
2016-03-05 09:53:17 -0800

    issues pic

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,5 +1,5 @@
 class Issue < ActiveRecord::Base
-  has_attached_file :avatar, styles: { medium: "710x400#", thumb: "300x100#" }, default_url: "news/:styles/missing.jpg"
+  has_attached_file :avatar, styles: { medium: "710x473#", thumb: "300x100#" }, default_url: "news/:styles/missing.jpg"
     validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
     
     extend FriendlyId
